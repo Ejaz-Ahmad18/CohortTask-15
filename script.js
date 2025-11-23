9be70a43b4dd8b90fb4ae3759a1f100d3f38cf55
ejazahmad18 <numen.ejaz@gmail.com>
2025-11-23 17:18:40 +0530

    Create-random-div

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
-var a=document.createElement("h1")
-a.innerHTML="hello"
-var body=document.querySelector("body")
-body.appendChild(a)
+// var a=document.createElement("h1")
+// a.innerHTML="hello"
+// var body=document.querySelector("body")
+// body.appendChild(a)
+
+
+
+var main=document.querySelector('main')
+var btn=document.querySelector('button')
+
+btn.addEventListener("click", ()=>{
+var div=document.createElement('div')
+var x=Math.random()*100
+var y=Math.random()*100
+var c1=Math.floor(Math.random()*256)
+var c2=Math.floor(Math.random()*256)
+var c3=Math.floor(Math.random()*256)
+var c4=Math.floor(Math.random()*256)
+
+
+
+div.style.backgroundColor=`rgb(${c1},${c2},${c3},${c4})`
+div.style.height='100px'
+div.style.width='100px'
+div.style.position='absolute'
+
+
+div.style.top=x+'%'
+div.style.left=y+'%'
+
+
+
+main.appendChild(div)
+
+
+})
