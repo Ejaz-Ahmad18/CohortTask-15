18bfa0e005b7693612cc9f16fc79ad8dd4cf3bda
ejazahmad18 <numen.ejaz@gmail.com>
2025-11-23 17:28:39 +0530

    Create-random-h1

--- script.js
+++ script.js
@@ -5,11 +5,41 @@
 
 
 
+// var main=document.querySelector('main')
+// var btn=document.querySelector('button')
+
+// btn.addEventListener("click", ()=>{
+// var div=document.createElement('div')
+// var x=Math.random()*100
+// var y=Math.random()*100
+// var c1=Math.floor(Math.random()*256)
+// var c2=Math.floor(Math.random()*256)
+// var c3=Math.floor(Math.random()*256)
+// var c4=Math.floor(Math.random()*256)
+
+
+
+// div.style.backgroundColor=`rgb(${c1},${c2},${c3},${c4})`
+// div.style.height='100px'
+// div.style.width='100px'
+// div.style.position='absolute'
+
+
+// div.style.top=x+'%'
+// div.style.left=y+'%'
+
+
+
+// main.appendChild(div)
+
+
+// })
+
 var main=document.querySelector('main')
 var btn=document.querySelector('button')
 
 btn.addEventListener("click", ()=>{
-var div=document.createElement('div')
+var h1=document.createElement('h1')
 var x=Math.random()*100
 var y=Math.random()*100
 var c1=Math.floor(Math.random()*256)
@@ -19,18 +49,20 @@ var c4=Math.floor(Math.random()*256)
 
 
 
-div.style.backgroundColor=`rgb(${c1},${c2},${c3},${c4})`
-div.style.height='100px'
-div.style.width='100px'
-div.style.position='absolute'
+h1.style.backgroundColor=`rgb(${c1},${c2},${c3},${c4})`
+// h1.style.height='100px'
+// h1.style.width='100px'
+h1.style.color='white'
+h1.innerHTML='hello'
+h1.style.position='absolute'
 
 
-div.style.top=x+'%'
-div.style.left=y+'%'
+h1.style.top=x+'%'
+h1.style.left=y+'%'
 
 
 
-main.appendChild(div)
+main.appendChild(h1)
 
 
 })
